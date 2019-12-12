fe3621962c78d167a937b5f597d647f342d87e17
alivojdanian <ali_vojdanian@yahoo.com>
2019-12-12 16:54:51 +0200

    Dragon Game Project

--- Assets/Scripts/Dragoncontroller.cs
+++ Assets/Scripts/Dragoncontroller.cs
@@ -6,7 +6,7 @@ namespace Game
 {
     public class Dragoncontroller : MonoBehaviour
     {
-
+        // Creating game objects for geting it on screen and deleting when player enter wrong buttonn
         public GameObject fire;
         public GameObject fire1;
         public GameObject fire2;
@@ -14,12 +14,18 @@ namespace Game
         public GameObject fire4;
         public GameObject fire5;
         public GameObject fire6;
+        public Gamecontroller gamecontroller;
+        
         public GameObject display;
+
+        // Creating a variable to count the tries
+        
         private int tries;
+        // Creating an arry of fire objects 
         private GameObject[] parts;
         public AudioSource loseSound;
         
-
+        // Checking if the tries runs out or not
         public bool isfired 
         {
             get { return tries < 0; }
@@ -27,14 +33,11 @@ namespace Game
 
         private void Start()
         {
+            // Adding objects in to the array
             parts = new GameObject[] { fire, fire1, fire2, fire3, fire4, fire5, fire6 };
             Reset();
-
         }
-       // public void PlayLose()
-       // {
-            
-       // }
+       
         public void punish() 
         {
             if(tries >=0)
@@ -44,20 +47,23 @@ namespace Game
                 parts[tries--].SetActive(true);
             }
         }
+
+        // Exicuted on every wrong answer
         public void Reset()
         {
+        
+            
+            // Checking if the parts array is null
             if(parts == null)
             {
-                
+                gamecontroller.scoreIndicator.text = "0";
                 return;
             }
             tries = parts.Length - 1;
-
             foreach (GameObject g in parts)
             {
-                
+                //
                 g.SetActive(false);
-
             }
         }
     }
--- Assets/Scripts/Gamecontroller.cs
+++ Assets/Scripts/Gamecontroller.cs
@@ -10,38 +10,50 @@ namespace Game
 { 
 public class Gamecontroller : MonoBehaviour
 {
+        // Three text to one for words one for latter in word one for the score
         public Text wordIndicator;
         public Text scoreIndicator;
         public Text letterIndicator;
+
         public AudioSource correctAnswer; 
         public AudioSource gameWin;
-
-
+        
+        // Creating an object of dragoncontroller class
         private Dragoncontroller Dragon;
+
         private string word;
+        
+        // Creating an arry of the charecters
         private char[] reveled;
+
+        // Variable for the score and completed
         private int score;
         private bool completed;
     public void Start()
     {
+            // Assinging an unity object to the dragon object of the dragon controller class
             Dragon = GameObject.FindGameObjectWithTag("Player").GetComponent<Dragoncontroller>();
-            reset();    
+            
+            // calling reset method
+            reset();   
     }
 
  
     void Update()
     {
+            string s = Input.inputString;
+
+            // Reviling the right answer for player to see but only if the game is completed 
             if (completed)
             {
-
-                string tmp = Input.inputString; 
-                if(Input.anyKeyDown)
-                next();
+                // Creating a temporary string for to save the user input
+                string tmp = Input.inputString;
+                if (Input.anyKeyDown)
+                    next();
                 return;
             }
-            
-          
-            string s = Input.inputString;
+            // Getting valid input from user
+
             if (s.Length == 1 && Textutil.isAlpha(s[0]))
             {
                 if (!check(s.ToUpper()[0]))
@@ -50,12 +62,16 @@ public class Gamecontroller : MonoBehaviour
 
                     if (Dragon.isfired)
                     {
+                        //Reveling each latter from user
                         wordIndicator.text = word;
                         completed = true;
                     }
                 }
             }
+
+            
     }
+        // Checking each user input
         private bool check(char c)
         {
             
@@ -63,11 +79,12 @@ public class Gamecontroller : MonoBehaviour
             int score = 0;
             bool ret = false;
 
-            for (int i =0; i<reveled.Length; i++)
+            for (int i =0; i < reveled.Length; i++)
             {
+
                 if (c == word[i])
                 {
-                    /*check if the charecter is valid or not*/
+                    //check if the charecter is valid or not
                     ret = true;
                     if (reveled[i] == 0)
                     {
@@ -75,13 +92,14 @@ public class Gamecontroller : MonoBehaviour
                         score++;
                     }
                 }
-                /*check if the game is completed*/
+                //check if the game is completed*
                 if (reveled[i] != 0)
                 {
                     completed++;
                 }
                 if(score != 0)
                 {
+                    // If all carecters are reveled
                     this.score += score;
                     if (completed == reveled.Length)
                     {
@@ -127,7 +145,7 @@ public class Gamecontroller : MonoBehaviour
             word = word.ToUpper();
             this.word = word;
 
-            /*reveling charecters */
+           //reveling charecters
             reveled = new char[word.Length];
             letterIndicator.text = "Letters = " + word.Length;
             
@@ -135,7 +153,9 @@ public class Gamecontroller : MonoBehaviour
         }
         public void next()
         {
+            // If the player runs out of tries reset the game
             Dragon.Reset();
+
             completed = false;
             setword(Wordlist.instance.next(0));
         }
